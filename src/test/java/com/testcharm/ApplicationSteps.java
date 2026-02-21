3516e40d2fe5d0b7990ed836d9761ecc49d6a77f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2026-02-21 15:23:05 +0800

    :white_check_mark: add first empty feature file

---
+++ src/test/java/com/testcharm/ApplicationSteps.java
@@ -0,0 +1,22 @@
+package com.testcharm;
+
+import com.github.leeonky.dal.Assertions;
+import io.cucumber.java.Before;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = {CucumberConfiguration.class}, loader = SpringBootContextLoader.class)
+@CucumberContextConfiguration
+public class ApplicationSteps {
+
+    @Value("${testcharm.dal.dumpinput:true}")
+    private boolean dalDumpInput;
+
+    @Before
+    public void disableDALDump() {
+        Assertions.dumpInput(dalDumpInput);
+    }
+
+}
---
+++ src/test/resources/features/process_kb.feature
@@ -0,0 +1,2 @@
+# language: zh-CN
+功能: 知识库处理
